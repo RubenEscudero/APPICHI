9b7fc33fe50155b532eaf6665e1031b3b3d4c0fb
Ruben Escudero <rubenescudero00@outlook.com>
2024-06-19 23:31:36 +0200

    create mobility and home base page

---
+++ src/Views/Home.xaml.cs
@@ -0,0 +1,9 @@
+namespace APPICHI.Views;
+
+public partial class Home : ContentPage
+{
+	public Home()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Views/Mobility.xaml.cs
@@ -0,0 +1,9 @@
+namespace APPICHI.Views;
+
+public partial class Mobility : ContentPage
+{
+	public Mobility()
+	{
+		InitializeComponent();
+	}
+}
