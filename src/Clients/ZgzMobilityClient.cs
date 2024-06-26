e22caa9eff2819e7fb189ea7c513fb7aeff54671
Rubén Escudero Tomás <rubenescudero00@outlook.com>
2024-06-26 22:14:58 +0200

    start to do ZgzMobilityClient

---
+++ src/Clients/ZgzMobilityClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPICHI.Clients
+{
+    static readonly String BaseAddress = "";
+    internal class ZgzMobilityClient
+    {
+    }
+}
