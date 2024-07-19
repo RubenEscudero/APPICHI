ce506986078c8a101665bc2fc3d759d054c7cdd0
Ruben Escudero <rubenescudero00@outlook.com>
2024-07-19 20:47:17 +0200

    format result time

---
+++ src/Models/ZgzTimeResultModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPICHI.Models
+{
+    public class ZgzTimeResultModel
+    {
+        public string? type { get; set; }
+        public List<Feature>? Features { get; set; }
+    }
+
+    public class Feature
+    {
+        public string? type { get; set; }
+        public Properties? Properties { get; set; }
+    }
+
+    public class Properties
+    {
+        public string? id { get; set; }
+        public string? title { get; set; }
+        public string? lastUpdated { get; set; }
+        public string? link { get; set; }
+        public string? icon { get; set; }
+        public List<Destino>? Destinos { get; set; }
+        public string? gtfsId { get; set; }
+    }
+
+    public class Destino
+    {
+        public string? linea { get; set; }
+        public string? destino { get; set; }
+        public string? primero { get; set; }
+        public string? segundo { get; set; }
+    }
+}
--- src/Views/ZgzMobilityPage.xaml.cs
+++ src/Views/ZgzMobilityPage.xaml.cs
@@ -1,4 +1,6 @@
 using APPICHI.Clients;
+using APPICHI.Models;
+using Newtonsoft.Json;
 
 namespace APPICHI.Views;
 
@@ -12,11 +14,22 @@ public partial class ZgzMobilityPage : ContentPage
 	async void GetBusByMarqueeId(object sender, EventArgs args)
 	{
 		string TimeResult = await ZgzMobilityClient.GetTimeByMarqueeId(MarqueeId.Text);
-		if (string.IsNullOrEmpty(TimeResult))
-		{
-			Result.Text = "No se ha podido encontrar el poste";
-		}
 
-		Result.Text = TimeResult;
-	}
+        if (string.IsNullOrEmpty(TimeResult))
+        {
+            Result.Text = "No se ha podido encontrar el poste";
+        }
+        else
+        {
+            ZgzTimeResultModel zgzTimeResult = JsonConvert.DeserializeObject<ZgzTimeResultModel>(TimeResult);
+            string FormatResult = "";
+            foreach (var destino in zgzTimeResult.Features[0].Properties.Destinos)
+            {
+                FormatResult = FormatResult + $"Linea: {destino.linea}, Destino: {destino.destino}," +
+                    $" Primero: {destino.primero}, Segundo: {destino.segundo} \n";
+            }
+            Result.Text = FormatResult;
+
+        }
+    }
 }
